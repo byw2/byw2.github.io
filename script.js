5e007db8fc396ee1130ac61a4c17080978483c71
Brendan Wang <byw2@princeton.edu>
2021-08-24 12:09:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $(".exp-organization")
+        .find("i")
+        .click(function () {
+            $(this).parent().next(".org-descrip").toggle();
+        });
+});
+
+// list of texts
+const texts = ["Hi, I'm Brendan!"];
+let count = 0;
+let index = "";
+let currentText = "";
+let letter = "";
+let typing = document.querySelector(".typing");
+
+(function type() {
+    // compares to count to length of array
+    if (count === texts.length) {
+        return;
+    }
+    currentText = texts[count];
+    // substring -- adding one more character to value
+    letter = currentText.slice(0, ++index);
+
+    typing.textContent = letter;
+
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+
+    setTimeout(type, 200);
+})();
